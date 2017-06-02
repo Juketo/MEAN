4e36d601941d43fc3dad2ed0fcb0f52e481dcea9
Julio Maciel <juketo@gmail.com>
2017-06-02 16:32:29 -0300

    cap3 - rota/verb Get

--- config/express.js
+++ config/express.js
@@ -6,5 +6,18 @@ var app = express();
 // através de app.use, podemos usar vários middlewares
 app.use(express.static('./public'));
 
+// para deixar as fotos acessíveis no client (via :3000)
+// configura o módulo alurapic/config/express.js, criando a URL 
+// http://localhost:3000/v1/fotos e retornando uma lista de fotos
+app.get('/v1/fotos', function(req, res){
+
+    var fotos = [
+        {_id: 1, titulo: 'Leão', url:'http://www.fundosanimais.com/Minis/leoes.jpg' },
+        {_id: 2, titulo: 'Leão 2', url:'http://www.fundosanimais.com/Minis/leoes.jpg' }
+    ];
+
+    res.json(fotos);
+});
+
 // exportando nossa instância do Express
 module.exports = app;
